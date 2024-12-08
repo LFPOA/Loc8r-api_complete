6120fcd50600b4e7fbf869c910b3eee319c289c9
LFPOA <150404536+LFPOA@users.noreply.github.com>
2024-12-08 14:33:47 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
-const passport = require('passport');
+const passport = require('passport'); // Required before the database models
 require('./app_api/models/db');
-require('./app_api/config/passport');
+require('./app_api/config/passport'); // Configuration after the database models
 
 // var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -32,27 +33,26 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_public', 'build/')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build/browser')));
 app.use(passport.initialize());
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
-app.get('*', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'app_public', 'build/', 'index.html'));
- });
+// app.get('*', function(req, res, next) {
+//   res.sendFile(path.join(__dirname, 'app_public', 'build/browser', 'index.html'));
+// });
 
 app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'app_public', 'build/', 'index.html'));
+  res.sendFile(path.join(__dirname, 'app_public', 'build/browser', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
-  if(err.name === 'UnauthorizedError') {
-    res
-      .status(401)
-      .json({"message": err.name + ": " + err.message});
+  if(err.name === 'UnauthorizedError'){
+    res.status(401).json({"message":err.name + ": " +err.message});
   }
 });
 
